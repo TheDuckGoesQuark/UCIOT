6f475fd5670004f24b961bbcc2965a08946d1ca4
Jordan Mackie <jm354@st-andrews.ac.uk>
2018-10-02 19:11:34 +0100

    WE RECEIVING MESSAGES BOYS

--- uciot/__main__.py
+++ uciot/__main__.py
@@ -10,12 +10,11 @@ class ListeningThread(threading.Thread):
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         print("Attempting to bind to socket")
-        # sock.bind((config.UDP_IP, config.UDP_PORT))
         sock.bind(('', config.UDP_PORT))
         print("Successfully bound socket")
 
         # sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, config.MULTICAST_HOPS)
-        # sock.setsockopt(socket.IPPROTO_IPV6, socket.IP_DEFAULT_MULTICAST_LOOP, 0)
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
 
         print("Beginning listening")
         while True:
@@ -26,10 +25,13 @@ class ListeningThread(threading.Thread):
 class SendingThread(threading.Thread):
     def run(self):
         print("Beginning sending")
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
         for i in range(5):
             time.sleep(2)
-            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
             sock.sendto(config.MESSAGE.encode('utf-8'), (config.UDP_IP, config.UDP_PORT))
+
+        sock.close()
 
         print("Finished sending")
 
--- uciot/config.py
+++ uciot/config.py
@@ -1,13 +1,12 @@
-import os
+import ipaddress
 
-LINK_LOCAL_MULTICAST = "FF02"
+LINK_LOCAL_MULTICAST = "FF01"
 HEX_USER_ID = "4EA3"
-UDP_IP = LINK_LOCAL_MULTICAST + ":" + HEX_USER_ID + "::2"
+UDP_IP = LINK_LOCAL_MULTICAST + "::1".decode('utf-8')
+
 UDP_PORT = 8080
 MESSAGE = "Hello World!"
 MULTICAST_HOPS = 3
 
 print("UDP target IP: {}".format(UDP_IP))
 print("UDP target port: {}".format(UDP_PORT))
-
-
