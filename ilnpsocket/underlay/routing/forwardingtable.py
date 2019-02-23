a56cfdf39c23f9a70359d8f5d3e0a392901b66a8
Jordan Mackie <jmackie97@hotmail.com>
2019-02-23 15:38:10 +0000

    Add age countdown to forwarding table, and return multiple locators when requesting next hop

--- ilnpsocket/underlay/routing/forwardingtable.py
+++ ilnpsocket/underlay/routing/forwardingtable.py
@@ -7,6 +7,7 @@ class ForwardingTable:
     Forwarding table stores the next hops for destination locators. It is routinely cleared and so will require
     updating.
     """
+
     def __init__(self, refresh_delay_secs):
         self.entries = {}
         self.refresh_thread = RefreshTableThread(refresh_delay_secs, self)
@@ -20,7 +21,10 @@ class ForwardingTable:
         return self.entries[locator]
 
     def add_entry(self, destination_locator, next_hop_locator, cost):
-        self.entries[destination_locator] = ForwardingEntry(next_hop_locator, cost)
+        if destination_locator in self.entries:
+            self.entries[destination_locator] = [ForwardingEntry(next_hop_locator, cost)]
+        else:
+            self.entries[destination_locator].append([ForwardingEntry(next_hop_locator, cost)])
 
     def record_entry(self, dest_locator, arriving_locator, route_cost):
         """
@@ -41,12 +45,14 @@ class ForwardingTable:
 
     def find_next_hops(self, packet_dest_locator):
         if self.has_entry_for(packet_dest_locator):
-            return [self.retrieve_entry_for(packet_dest_locator).next_hop_locator]
+            return self.retrieve_entry_for(packet_dest_locator).next_hop_locator
         else:
             return []
 
-    def clear_table(self):
-        self.entries.clear()
+    def decrement_and_clear(self):
+        for entry_list in self.entries.values():
+            entry_list[:] = map(lambda x: x.decrement_ltl(), entry_list)
+            entry_list[:] = filter(lambda x: x.left_to_live > 0, entry_list)
 
     def print_contents(self):
         print("INFO - Current state of forwarding table before refreshing:")
@@ -65,7 +71,7 @@ class RefreshTableThread(threading.Thread):
     def run(self):
         while self.running:
             self.routing_table.print_contents()
-            self.routing_table.clear_table()
+            self.routing_table.decrement_and_clear()
             time.sleep(self.refresh_delay)
 
     def stop(self):
@@ -76,12 +82,23 @@ class ForwardingEntry:
     def __init__(self, next_hop_locator, cost):
         self.cost = cost
         self.next_hop_locator = next_hop_locator
+        self.left_to_live = 10
 
     def should_be_replaced_by(self, route_cost):
         """A lower cost, or  equally good but more recent route cost will be preferred."""
-        return self.cost >= route_cost
+        if self.left_to_live == 0:
+            return True
+        else:
+            return self.cost >= route_cost
 
     def change_path(self, new_next_hop, new_cost):
         """Updates next hop and cost for this destination entry"""
         self.next_hop_locator = new_next_hop
         self.cost = new_cost
+        self.reset_ltl()
+
+    def reset_ltl(self):
+        self.left_to_live = 10
+
+    def decrement_ltl(self):
+        self.left_to_live = self.left_to_live - 1
