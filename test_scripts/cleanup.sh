bfd24434c64ca2ff8b4529a83b516eb20f511a27
Jordan Mackie <jmackie97@hotmail.com>
2019-02-25 21:43:03 +0000

    Add killswitch to cleanup script

--- test_scripts/cleanup.sh
+++ test_scripts/cleanup.sh
@@ -8,14 +8,6 @@ mapfile -t machines < ${machine_list_file}
 
 nMachines=${#machines[@]}
 
-for i in "${!machines[@]}"; do
-    ssh -o ConnectTimeout=3 jm354@${machines[i]} "export UCIOT_CONT=0;"
-    if [[ ${i} -eq ${nMachines} ]]; then
-        break;
-    fi
-done
-
-echo "Waiting..."
-wait
+mkdir ~/killswitch
 
 
