40882f6e2769c1ddcbc36d794eec47d560e7f7f8
Jordan Mackie <jmackie97@hotmail.com>
2019-02-25 21:43:18 +0000

    Add improved killswitch

--- test_scripts/cleanup.sh
+++ test_scripts/cleanup.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-nMachines=40
-machine_list_file="up_machines.txt"
-
-# Read contents of file into array
-mapfile -t machines < ${machine_list_file}
-
-nMachines=${#machines[@]}
-
 mkdir ~/killswitch
 
 
