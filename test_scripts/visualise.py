b0bb8f1af46b904e81b1728e17b2bee0084f3afd
Jordan Mackie <jmackie97@hotmail.com>
2019-03-11 17:36:01 +0000

    Add colourmap at periodic snapshots of packets sent by each node

--- test_scripts/visualise.py
+++ test_scripts/visualise.py
@@ -3,6 +3,15 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def get_snapshot_index(time_value, snapshot_start_times):
+    for idx in range(len(snapshot_start_times) - 1, 0, -1):
+        if time_value >= snapshot_start_times[idx]:
+            return idx
+
+    return 0
+
+
 ap = argparse.ArgumentParser()
 ap.add_argument("data_file", help="CSV file containing experiment data")
 
@@ -13,14 +22,36 @@ data = pd.read_csv(args.data_file)
 start_time = data["sent_at_time"].min()
 end_time = data["sent_at_time"].max()
 
-snapshots = np.linspace(start_time, end_time, 3)
+n_snapshots = 3
+bins = np.linspace(start_time, end_time, n_snapshots)
 
-data["binned"] = pd.cut(data["sent_at_time"], snapshots)
-layout = [[x for x in range(y, y + 4)] for y in range(1, 17, 4)]
-print(layout)
+grouped_by_node = data.groupby("node_id")
 
-fig, ax = plt.subplots()
-im = ax.imshow(layout)
+n_cols = 4
+n_rows = 4
+layout = [[0 for col in range(0, n_cols)] for row in range(0, n_rows)]
 
-fig.tight_layout()
-plt.show()
+snapshots = [layout.copy() for x in range(n_snapshots)]
+
+for node_id, group in grouped_by_node:
+    if node_id == 17:
+        continue
+    for idx, row in group.iterrows():
+        snapshot_index = get_snapshot_index(row.sent_at_time, bins)
+        heatmap_row = int((node_id - 1) / 4)
+        heatmap_col = (node_id - 1) % 4
+        snapshots[snapshot_index][heatmap_row][heatmap_col] = snapshots[snapshot_index][heatmap_row][heatmap_col] + 1
+
+for idx, snapshot in enumerate(snapshots):
+    fig, ax = plt.subplots()
+    im = ax.imshow(snapshots[idx])
+    ax.set_title("Packets sent during snapshot {}".format(idx + 1))
+    fig.colorbar(im)
+
+    # Label with node ID
+    for i in range(len(layout)):
+        for j in range(len(layout)):
+            text = ax.text(j, i, (i * 4 + j) + 1,
+                           ha="center", va="center", color="w")
+
+    fig.show()
