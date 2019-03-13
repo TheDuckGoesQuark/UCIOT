80a5b137bdc65b4fdd140a4a53a7ba1ea96ebe99
Jordan Mackie <jmackie97@hotmail.com>
2019-03-13 18:38:35 +0000

    Add type hint to listeningsocket

--- ilnpsocket/underlay/sockets/listeningsocket.py
+++ ilnpsocket/underlay/sockets/listeningsocket.py
@@ -6,33 +6,30 @@ class ListeningSocket:
     """Wrapper for socket instance that listens for traffic from a specific
     multicast group and provides mapping from locator to ipv6 address"""
 
-    def __init__(self, multicast_address, port, locator):
+    def __init__(self, multicast_address: str, port: int, locator: int):
         """
         Creates instance of listening socket
         :param multicast_address: multicast address this socket should accept traffic from
         :param port: port number this socket should accept traffic from
         :param locator: ILNP locator value this socket is the interface for
         """
-        self.multicast_address = multicast_address
-        self.__port = port
-        self.__sock = create_listening_socket(port, multicast_address)
-        self.locator = locator
+        self.multicast_address: str = multicast_address
+        self.__port: int = port
+        self.__sock: socket.socket = create_listening_socket(port, multicast_address)
+        self.locator: int = locator
 
     def fileno(self):
         """Provides direct access to socket file handle for select module"""
         return self.__sock.fileno()
 
-    def recvfrom_into(self, buffer, buffer_size=None):
+    def recvfrom_into(self, buffer: bytearray, buffer_size: int = None):
         if buffer_size is None:
             buffer_size = len(buffer)
 
         return self.__sock.recvfrom_into(buffer, buffer_size)
 
-    def getsockname(self):
-        return self.__sock.getsockname()
 
-
-def create_listening_socket(port, multicast_address):
+def create_listening_socket(port: int, multicast_address: str) -> socket.socket:
     """
     Creates a UDP datagram socket bound to listen for traffic from the given
     multicast address.
